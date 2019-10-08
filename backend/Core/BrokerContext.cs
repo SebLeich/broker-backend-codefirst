42c07a0994575409829881aa36aba671da538e6b
SebLeich <sebleichsenring@gmail.com>
2019-10-08 12:46:15 +0200

    broker context added

---
+++ backend/Core/BrokerContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace backend.Core
+{
+    /// <summary>
+    /// the context contains the database's model
+    /// </summary>
+    public class BrokerContext : DbContext
+    {
+        /// <summary>
+        /// the service relation
+        /// </summary>
+        public DbSet<Service> Services { get; set; }
+    }
+}
